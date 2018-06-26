df37854f575f65b08b25ca59e372fcbeb1bf9ee1
David Hopkins <dlhopkins2@gmail.com>
2018-06-26 01:14:37 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ require 'pry'
 
 def create_project_hash
   projects = {}
- 
+
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
